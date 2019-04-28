2fff59337fbb04afd8b6ae4fc623792ca6684380
Antoine Comte <antoine@comte.cc>
2019-04-28 13:35:15 +0200

    add smooth scrolling

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,13 @@ $('#alert').click(() => {
 });
 
 // Your jQuery code
+
+
+$('.js-scrollTo').click(function() {
+  var page = $(this).attr('href');
+  var speed = 500; // Durée de l'animation (en ms)
+  $('html, body').animate({
+    scrollTop: $(page).offset().top
+  }, speed);
+  return false;
+});
